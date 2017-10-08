1f457937b74ceb942fb628d658401cb276476a01
aliem3x <32553199+aliem3x@users.noreply.github.com>
2017-10-08 19:49:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
             $frase1="Me gustan las manzanas"; //Ali
             $frase2="Los gatos dicen miau"; //Cristina
-            $frase3="Hola a todo el mundo"; //Joy
+
 
             echo $frase1;
             echo $frase2;
