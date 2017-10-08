de6ab73be2011f0a000975a1084edac9baf6f88c
cristinaa7x <32458218+cristinaa7x@users.noreply.github.com>
2017-10-08 19:41:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
        <?php
 
             $frase1="Me gustan las manzanas"; //Ali
-            $frase2="Los gatos dicen maiu"; //Cristina
+            $frase2="Los gatos dicen miau"; //Cristina
             $frase3="Hola a todo el mundo"; //Joy
 
             echo $frase1;
