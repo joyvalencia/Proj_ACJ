148904c78a636ef82ba10df90b1acb2faa3bd93f
joyvalencia <32458789+joyvalencia@users.noreply.github.com>
2017-10-08 19:40:09 +0200

    Update index.php
    frase3

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
        <?php
 
             $frase1="Me gustan las manzanas"; //Ali
-            $frase2="Los gatos dicen miau"; //Cristina
-            $frase3;
+            $frase2;
+            $frase3="Hola a todo el mundo"; //Joy
 
             echo $frase1;
             echo $frase2;
